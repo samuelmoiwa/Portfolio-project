050c3a3ecb916cc3b504d44f3f024903d272622a
Nedramrez <zermarden@gmail.com>
2022-09-05 21:49:22 +0600

    Add js code to make mobile menu disappear after clicking the link

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active")
 })
 
+document.querySelectorAll(".nav-link").forEach(n => 
+    n.addEventListener("click", () => 
+    { hamburger.classList.remove("active") 
+    navMenu.classList.remove("active") })) 
