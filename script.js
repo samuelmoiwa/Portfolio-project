3c9a53164da11cc224c569e84185106fcba3c835
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-05 15:38:11 +0000

    update script.js file

--- script.js
+++ script.js
@@ -5,11 +5,5 @@ const navMenu = document.querySelector(".nav-menu-ul")
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
-
-    let naveBackground = document.querySelector(".header-nav-container")
-    
-    if(hamburger === active && navMenu === active){
-      
-    }
 })
 
