949c4e2fea4d48bb0df8c5b14868e60d2cc55e44
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-05 15:36:33 +0000

    add javascript file, with menu item click listener

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const hamburger = document.querySelector(".menu-btn")
+const navMenu = document.querySelector(".nav-menu-ul")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+
+    let naveBackground = document.querySelector(".header-nav-container")
+    
+    if(hamburger === active && navMenu === active){
+      
+    }
+})
+
