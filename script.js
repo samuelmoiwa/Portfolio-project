09e3607f9959d7a76c36522e785086b0092f4d5b
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-08 15:16:34 +0000

    elslint error resolve on script.js

--- script.js
+++ script.js
@@ -175,5 +175,8 @@ const error = document.getElementById('error-message');
 const form = document.getElementById('form');
 error.style.visibility = 'hidden';
 function formValidate(event) {
-  if (email.value !== email.value.toLowerCase()) { error.style.visibility = 'visible'; error.innerHTML = 'Please enter your email address in lowercase.'; event.preventDefault(); } else { error.style.visibility = 'hidden'; } }
+  if (email.value !== email.value.toLowerCase()) { error.style.visibility = 'visible'; error.innerHTML = 'Please enter your email address in lowercase.'; event.preventDefault(); } else {
+    error.style.visibility = 'hidden';
+  }
+}
 form.addEventListener('submit', formValidate);
